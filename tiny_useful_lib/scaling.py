2ee9402f4ec2595d40f6dca732bc7bea49d7b558
andreykugut <andrey.kugut@gmail.com>
2024-10-02 19:48:27 +0300

    add weights

--- tiny_useful_lib/scaling.py
+++ tiny_useful_lib/scaling.py
@@ -36,11 +36,15 @@ def fluxonium_coup(f01, alpha, bounds=[(2, 100), (0.4, 1.5), (0.2, 6)]):
     
     return x[np.argmin(loss)]
 
-def fluxonium_search(f01, f12, f03, bounds=[(2, 100), (0.5, 1.5), (0.2, 6)]):
+def fluxonium_search(f01, f12, f03, bounds=[(2, 100), (0.5, 1.5), (0.2, 6)], weights=[1, 1, 1]):
 
+    
     def fun(x):
         eigval, _, _ = Fluxonium(x[0], x[1], x[2], F=0.5)
-        return (eigval[1] - f01)**2 + (eigval[2] - eigval[1] - f12)**2 + (eigval[3] - f03)**2
+        loss=weights[0]*(eigval[1] - f01)**2 +\
+             weights[1]*(eigval[2] - eigval[1] - f12)**2 +\
+             weights[2]*(eigval[3] - f03)**2
+        return loss
     
     opt1 = minimize(fun, x0=[4, 1.5, 4], bounds=bounds)
     opt2 = minimize(fun, x0=[3, 1, 4], bounds=bounds)
